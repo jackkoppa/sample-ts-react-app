8158ad86233cb9eea81535740a4e601d936f048c
Jack Koppa <hello@jackkoppa.com>
2019-02-17 21:45:41 -0500

    feature: Test out usage of Polaris components Incl. Badge & Tooltip, to test out TSX functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
 import React, { Component } from 'react';
+
+import { AppProvider, Badge, Link, Tooltip } from '@shopify/polaris'
+
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
+
   render() {
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+
+      <AppProvider>
+        <div className="App">
+          <header className="App-header">
+
+            <Tooltip content="explantion">
+              <Link>Some confusing text</Link>
+            </Tooltip>
+
+            <p>
+              Jack Koppa <Badge status="success"> A Status</Badge>
+            </p>
+
+
+
+
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Testing a create-react-app w/ TypeScript
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </AppProvider>
     );
   }
 }
